bhollis/jsonview

commit 756fa51855d1cdbbd62bac47606df162037f9dbe

#97 Support Firefox multiprocess (electrolysis, e10s)

lib/main.js
@@ -2,51 +2,22 @@
  * @author Benjamin Hollis
  * @author Quoc-Viet Nguyen
  */
-const { Cc, Ci, components } = require("chrome");
-const xpcom = require("sdk/platform/xpcom");
-const { JSONView } = require("./jsonview");
-const prefs = require("./prefs");
+const { Cu, Cc, Ci, components } = require("chrome");
 
-// This component is an implementation of nsIStreamConverter that converts
-// application/json to html
-const JSON_TYPE = "application/json";
-const CONTRACT_ID = "@mozilla.org/streamconv;1?from=" + JSON_TYPE + "&to=*/*";
-const CLASS_ID = "{64890660-53c4-11dd-ae16-0800200c9a66}";
-const GECKO_VIEWER = "Gecko-Content-Viewers";
+// Platform
+const globalMM = Cc["@mozilla.org/globalmessagemanager;1"].
+  getService(Ci.nsIMessageListenerManager);
 
-// Create and register the service
-var service = xpcom.Service({
-  id: components.ID(CLASS_ID),
-  contract: CONTRACT_ID,
-  Component: JSONView,
-  register: false,
-  unregister: false
-});
-
-// Hack to remove FF8+'s built-in JSON-as-text viewer
-var categoryManager = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
-var geckoViewer = categoryManager.getCategoryEntry(GECKO_VIEWER, JSON_TYPE);
+const url = module.uri.replace("main.js", "registrar.js");
 
 function onLoad(options, callbacks) {
-  console.debug(options.loadReason);
-  if (!xpcom.isRegistered(service)) {
-    xpcom.register(service);
-  }
-  // Remove built-in JSON viewer
-  categoryManager.deleteCategoryEntry(GECKO_VIEWER, JSON_TYPE, false);
-  
-  // Tell Firefox that .json files are application/json
-  categoryManager.addCategoryEntry('ext-to-type-mapping', 'json', 'application/json', false, true);
-  prefs.register();
+  globalMM.loadFrameScript(url, true);
+  globalMM.broadcastAsyncMessage("jsonview-onload");
 };
 
 function onUnload(reason) {
-  if (xpcom.isRegistered(service)) {
-    xpcom.unregister(service);
-  }
-  // Re-add built-in JSON viewer
-  categoryManager.addCategoryEntry(GECKO_VIEWER, JSON_TYPE, geckoViewer, false, false);
-  prefs.unregister();
+  globalMM.broadcastAsyncMessage("jsonview-onunload");
+  globalMM.removeDelayedFrameScript(url);
 };
 
 exports.main = onLoad;

lib/registrar.js
@@ -1,0 +1,66 @@
+/**
+ * @author Benjamin Hollis
+ * @author Quoc-Viet Nguyen
+ */
+
+let Cu = Components.utils;
+let { devtools } = Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
+let require = devtools.require;
+
+const { Cc, Ci, components } = require("chrome");
+const xpcom = require("sdk/platform/xpcom");
+const { JSONView } = require("resource://jsonview-at-brh-dot-numbera-dot-com/jsonview/lib/jsonview.js");
+const prefs = require("resource://jsonview-at-brh-dot-numbera-dot-com/jsonview/lib/prefs.js");
+
+// This component is an implementation of nsIStreamConverter that converts
+// application/json to html
+const JSON_TYPE = "application/json";
+const CONTRACT_ID = "@mozilla.org/streamconv;1?from=" + JSON_TYPE + "&to=*/*";
+const CLASS_ID = "{64890660-53c4-11dd-ae16-0800200c9a66}";
+const GECKO_VIEWER = "Gecko-Content-Viewers";
+
+// Create and register the service
+var service = xpcom.Service({
+  id: components.ID(CLASS_ID),
+  contract: CONTRACT_ID,
+  Component: JSONView,
+  register: false,
+  unregister: false
+});
+
+// Hack to remove FF8+'s built-in JSON-as-text viewer
+var categoryManager = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
+var geckoViewer;
+try {
+  geckoViewer = categoryManager.getCategoryEntry(GECKO_VIEWER, JSON_TYPE);
+} catch (err) {
+  // The category entry doesn't have to exist.
+}
+
+// Handle messages from the parent process.
+addMessageListener("jsonview-onload", onLoad);
+addMessageListener("jsonview-onunload", onUnload);
+
+function onLoad(options, callbacks) {
+  //console.debug(options.loadReason);
+  if (!xpcom.isRegistered(service)) {
+    xpcom.register(service);
+  }
+  // Remove built-in JSON viewer
+  categoryManager.deleteCategoryEntry(GECKO_VIEWER, JSON_TYPE, false);
+  
+  // Tell Firefox that .json files are application/json
+  categoryManager.addCategoryEntry('ext-to-type-mapping', 'json', 'application/json', false, true);
+  prefs.register();
+};
+
+function onUnload(reason) {
+  if (xpcom.isRegistered(service)) {
+    xpcom.unregister(service);
+  }
+  // Re-add built-in JSON viewer
+  if (geckoViewer) {
+    categoryManager.addCategoryEntry(GECKO_VIEWER, JSON_TYPE, geckoViewer, false, false);
+  }
+  prefs.unregister();
+};

commit 6fd4d627c1fcb9e29ddfa13d406c38be839d6141

Support user-defined additional content types to treat as JSON. This is a potentially dangerous option that may be reverted if it causes problems. Fixes #7.
This also fixes preferences handling that was broken in 756fa51855d1cdbbd62bac47606df162037f9dbe.

lib/main.js
@@ -8,17 +8,56 @@ const { Cu, Cc, Ci, components } = require("chrome");
 const globalMM = Cc["@mozilla.org/globalmessagemanager;1"].
   getService(Ci.nsIMessageListenerManager);
 
-const url = module.uri.replace("main.js", "registrar.js");
+// Append a timestamp to work around https://bugzilla.mozilla.org/show_bug.cgi?id=1051238
+const url = module.uri.replace("main.js", "registrar.js") + "?" + Date.now();
+
+const prefs = require("./prefs");
+const simplePrefs = require("sdk/simple-prefs");
 
 function onLoad(options, callbacks) {
   globalMM.loadFrameScript(url, true);
-  globalMM.broadcastAsyncMessage("jsonview-onload");
-};
+  globalMM.broadcastAsyncMessage("jsonview-onload", prefs.getJsonContentTypes());
+
+  prefs.register()
+  simplePrefs.on("alternateContentTypes", debounce(function() {
+    // Reload JSONView on content type change
+    globalMM.broadcastAsyncMessage("jsonview-onunload");
+    globalMM.broadcastAsyncMessage("jsonview-onload", prefs.getJsonContentTypes());
+  }, 1000));
+}
 
 function onUnload(reason) {
+  prefs.unregister();
   globalMM.broadcastAsyncMessage("jsonview-onunload");
   globalMM.removeDelayedFrameScript(url);
-};
+}
+
+// From Underscore, because preferences update on every keystroke!
+const { setTimeout } = require("sdk/timers");
+function debounce(func, wait) {
+  var timeout, args, context, timestamp, result;
+
+  var later = function() {
+    var last = Date.now() - timestamp;
+
+    if (last < wait && last >= 0) {
+      timeout = setTimeout(later, wait - last);
+    } else {
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    }
+  };
+
+  return function() {
+    context = this;
+    args = arguments;
+    timestamp = Date.now();
+    if (!timeout) timeout = setTimeout(later, wait);
+
+    return result;
+  };
+}
 
 exports.main = onLoad;
 exports.onUnload = onUnload;

lib/prefs.js
@@ -37,17 +37,38 @@ function onPrefChange(prefName) {
   }
 }
 
-function register() {
-  prefs.on("", onPrefChange);
+function register(onContentTypesChanged) {
+  prefs.on(JSON_HTTP_ACCEPT_PREF_NAME, onPrefChange);
 }
 
 function unregister() {
-  prefs.removeListener("", onPrefChange);
+  prefs.removeListener(JSON_HTTP_ACCEPT_PREF_NAME, onPrefChange);
   // Remove JSON from HTTP Accept header if it has been inserted
   if (prefs.prefs[JSON_HTTP_ACCEPT_PREF_NAME]) {
     saveHttpAcceptPref(false);
   }
 }
 
+// Get a list of MIME types we should treat as JSON. This will
+// always be a unique list that includes "application/json".
+function getJsonContentTypes() {
+  var contentTypes = prefs.prefs.alternateContentTypes.split(/[,\s]+/);
+  contentTypes.push("application/json");
+
+  var unique = {};
+  contentTypes = contentTypes
+    // reject non-mime-type-looking stuff
+    .filter(function(c) {
+      return (c != '') && /\w+\/\w+/.test(c);
+    })
+    // unique
+    .filter(function(e) {
+      return !(e in unique) && (unique[e] = true);
+    });
+
+  return contentTypes;
+}
+
 exports.register = register;
 exports.unregister = unregister;
+exports.getJsonContentTypes = getJsonContentTypes;

lib/registrar.js
@@ -13,58 +13,68 @@ let basePref = prefsService.get('extensions.jsonview@brh.numbera.com.sdk.baseURI
 const { Cc, Ci, components } = require("chrome");
 const xpcom = require("sdk/platform/xpcom");
 
-// This component is an implementation of nsIStreamConverter that converts
-// application/json to html
-const JSON_TYPE = "application/json";
-const CONTRACT_ID = "@mozilla.org/streamconv;1?from=" + JSON_TYPE + "&to=*/*";
-const CLASS_ID = "{64890660-53c4-11dd-ae16-0800200c9a66}";
+const { JSONView } = require(basePref + "lib/jsonview.js");
+const categoryManager = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
+
 const GECKO_VIEWER = "Gecko-Content-Viewers";
 
-const { JSONView } = require(basePref + "lib/jsonview.js");
-const prefs = require(basePref+"lib/prefs.js");
-
-var service = xpcom.Service({
-  id: components.ID(CLASS_ID),
-  contract: CONTRACT_ID,
-  Component: JSONView,
-  register: false,
-  unregister: false
-});
+var services = [];
 
 // Handle messages from the parent process.
 addMessageListener("jsonview-onload", onLoad);
 addMessageListener("jsonview-onunload", onUnload);
 
 function onLoad(options, callbacks) {
-  // Create and register the service
+  const contentTypes = options.data;
+  contentTypes.forEach(function(contentType) {
+    // Get a reference to the existing Gecko viewer for this content type, if it exists
+    var geckoViewer;
+    try {
+      geckoViewer = categoryManager.getCategoryEntry(GECKO_VIEWER, contentType);
+    } catch (err) {
+      // The category entry doesn't have to exist.
+    }
 
-  // Hack to remove FF8+'s built-in JSON-as-text viewer
-  var categoryManager = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
-  var geckoViewer;
-  try {
-    geckoViewer = categoryManager.getCategoryEntry(GECKO_VIEWER, JSON_TYPE);
-  } catch (err) {
-    // The category entry doesn't have to exist.
-  }
+    // Create and register the a service for each content type
+    // This component is an implementation of nsIStreamConverter that converts
+    // application/json to html
+    var contractId = "@mozilla.org/streamconv;1?from=" + contentType + "&to=*/*";
+    var service = xpcom.Service({
+      contract: contractId,
+      Component: JSONView,
+      register: false,
+      unregister: false
+    });
 
-  if (!xpcom.isRegistered(service)) {
-    xpcom.register(service);
-  }
-  // Remove built-in JSON viewer
-  categoryManager.deleteCategoryEntry(GECKO_VIEWER, JSON_TYPE, false);
+    // Keep track of everything so we can undo it in onUnload
+    services.push({
+      service: service,
+      contentType: contentType,
+      geckoViewer: geckoViewer
+    });
+
+    if (!xpcom.isRegistered(service)) {
+      xpcom.register(service);
+    }
+
+    // Remove built-in JSON viewer
+    categoryManager.deleteCategoryEntry(GECKO_VIEWER, contentType, false);
+  });
 
   // Tell Firefox that .json files are application/json
   categoryManager.addCategoryEntry('ext-to-type-mapping', 'json', 'application/json', false, true);
-  prefs.register();
 };
 
 function onUnload(reason) {
-  if (xpcom.isRegistered(service)) {
-    xpcom.unregister(service);
-  }
-  // Re-add built-in JSON viewer
-  if (geckoViewer) {
-    categoryManager.addCategoryEntry(GECKO_VIEWER, JSON_TYPE, geckoViewer, false, false);
-  }
-  prefs.unregister();
+   services.forEach(function(serviceInfo) {
+    if (xpcom.isRegistered(serviceInfo.service)) {
+      xpcom.unregister(serviceInfo.service);
+    }
+
+    // Re-add built-in JSON viewer
+    if (serviceInfo.geckoViewer) {
+      categoryManager.addCategoryEntry(GECKO_VIEWER, serviceInfo.contentType, serviceInfo.geckoViewer, false, false);
+    }
+  });
+  services = [];
 };