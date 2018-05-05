406497707d1153dd2e1e3964de1d928816044a88
Ben Hollis <ben@benhollis.net>
2018-05-04 19:39:36 -0700

    JSONView kinda works!

---
+++ lib/jsonformatter.ts
@@ -0,0 +1,198 @@
+/**
+ * The JSONFormatter helper module. This contains two major functions, jsonToHTML and errorPage,
+ * each of which returns an HTML document.
+ */
+
+/** Convert a whole JSON value / JSONP response into a formatted HTML document */
+export function jsonToHTML(json: any, uri: string) {
+  const output = `<div id="json">${valueToHTML(json, '<root>')}</div>`;
+  return toHTML(output, uri);
+}
+
+/** Produce an error document for when parsing fails. */
+export function errorPage(error: Error, data: string, uri: string) {
+  // Escape unicode nulls
+  data = data.replace("\u0000", "\uFFFD");
+
+  const errorInfo = massageError(error);
+
+  let output = `<div id="error">${_('errorParsing')}`;
+  if (errorInfo.message) {
+    output += `<div class="errormessage">${errorInfo.message}</div>`;
+  }
+  output += `</div><div id="json">${highlightError(data, errorInfo.line, errorInfo.column)}</div>`;
+  return toHTML(output, uri + ' - Error');
+}
+
+// TODO: add localization
+function _(str: string) {
+  return str;
+}
+
+/**
+ * Encode a string to be used in HTML
+ */
+function htmlEncode(t: any): string {
+  return (typeof t !== "undefined" && t !== null) ? t.toString()
+    .replace(/&/g, "&amp;")
+    .replace(/"/g, "&quot;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    : '';
+}
+
+/**
+ * Completely escape a json string
+ */
+function jsString(s: string): string {
+  // Slice off the surrounding quotes
+  s = JSON.stringify(s).slice(1, -1);
+  return htmlEncode(s);
+}
+
+/**
+ * Is this a valid "bare" property name?
+ */
+function isBareProp(prop: string): boolean {
+  return /^[A-Za-z_$][A-Za-z0-9_\-$]*$/.test(prop);
+}
+
+/**
+ * Surround value with a span, including the given className
+ */
+function decorateWithSpan(value: any, className: string) {
+  return `<span class="${className}">${htmlEncode(value)}</span>`;
+}
+
+// Convert a basic JSON datatype (number, string, boolean, null, object, array) into an HTML fragment.
+function valueToHTML(value: any, path: string) {
+  const valueType = typeof value;
+
+  if (value === null) {
+    return decorateWithSpan('null', 'null');
+  } else if (Array.isArray(value)) {
+    return arrayToHTML(value, path);
+  } else if (valueType === 'object') {
+    return objectToHTML(value, path);
+  } else if (valueType === 'number') {
+    return decorateWithSpan(value, 'num');
+  } else if (valueType === 'string' &&
+            value.charCodeAt(0) === 8203 &&
+            !isNaN(value.slice(1))) {
+    return decorateWithSpan(value.slice(1), 'num');
+  } else if (valueType === 'string') {
+    if (/^(http|https|file):\/\/[^\s]+$/i.test(value)) {
+      return `<a href="${htmlEncode(value)}"><span class="q">&quot;</span>${jsString(value)}<span class="q">&quot;</span></a>`;
+    } else {
+      return `<span class="string">&quot;${jsString(value)}&quot;</span>`;
+    }
+  } else if (valueType === 'boolean') {
+    return decorateWithSpan(value, 'bool');
+  }
+
+  return '';
+}
+
+// Convert an array into an HTML fragment
+function arrayToHTML(json: any, path: string) {
+  if (json.length === 0) {
+    return '[ ]';
+  }
+
+  let output = '';
+  for (let i = 0; i < json.length; i++) {
+    const subPath = `${path}[${i}]`;
+    output += '<li>' + valueToHTML(json[i], subPath);
+    if (i < json.length - 1) {
+      output += ',';
+    }
+    output += '</li>';
+  }
+  return (json.length === 0 ? '' : '<span class="collapser"></span>') +
+    `[<ul class="array collapsible">${output}</ul>]`;
+}
+
+// Convert a JSON object to an HTML fragment
+function objectToHTML(json: any, path: string) {
+  let numProps = Object.keys(json).length;
+  if (numProps === 0) {
+    return '{ }';
+  }
+
+  let output = '';
+  for (const prop in json) {
+    let subPath = '';
+    let escapedProp = JSON.stringify(prop).slice(1, -1);
+    const bare = isBareProp(prop);
+    if (bare) {
+      subPath = `${path}.${escapedProp}`;
+    } else {
+      escapedProp = `"${escapedProp}"`;
+    }
+    output += `<li><span class="prop${(bare ? '' : ' quoted')}" title="${htmlEncode(subPath)}"><span class="q">&quot;</span>${jsString(prop)}<span class="q">&quot;</span></span>: ${valueToHTML(json[prop], subPath)}`;
+    if (numProps > 1) {
+      output += ',';
+    }
+    output += '</li>';
+    numProps--;
+  }
+
+  return `<span class="collapser"></span>{<ul class="obj collapsible">${output}</ul>}`;
+}
+
+// Clean up a JSON parsing error message
+function massageError(error: Error): {
+  message: string;
+  line?: number;
+  column?: number;
+} {
+  if (!error.message) {
+    return error;
+  }
+
+  const message = error.message.replace(/^JSON.parse: /, '').replace(/of the JSON data/, '');
+  const parts = /line (\d+) column (\d+)/.exec(message);
+  if (!parts || parts.length !== 3) {
+    return error;
+  }
+
+  return {
+    message: htmlEncode(message),
+    line: Number(parts[1]),
+    column: Number(parts[2])
+  };
+}
+
+function highlightError(data: string, lineNum?: number, columnNum?: number) {
+  if (!lineNum || !columnNum) {
+    return htmlEncode(data);
+  }
+
+  const lines = data.match(/^.*((\r\n|\n|\r)|$)/gm)!;
+
+  let output = '';
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+
+    if (i === lineNum - 1) {
+      output += '<span class="errorline">';
+      output += `${htmlEncode(line.substring(0, columnNum - 1))}<span class="errorcolumn">${htmlEncode(line[columnNum - 1])}</span>${htmlEncode(line.substring(columnNum))}`;
+      output += '</span>';
+    } else {
+      output += htmlEncode(line);
+    }
+  }
+
+  return output;
+}
+
+// Wrap the HTML fragment in a full document. Used by jsonToHTML and errorPage.
+function toHTML(content: string, title: string) {
+  return `<!DOCTYPE html>
+<html><head><title>${htmlEncode(title)}</title>
+<link rel="stylesheet" type="text/css" href="${browser.extension.getURL("data/default.css")}">
+<script type="text/javascript" src="${browser.extension.getURL("data/default.js")}"></script>
+</head><body>
+${content}
+</body></html>`;
+}
--- lib/main.ts
+++ lib/main.ts
@@ -1,18 +1,20 @@
+import { jsonToHTML, errorPage } from "./jsonformatter";
+
 const jsonRequestIds = new Set<string>();
 
 interface RequestDetails {
-  requestId: string,
-  url: string,
-  method: string,
-  frameId: number,
-  parentFrameId: number,
-  tabId: number,
-  type: browser.webRequest.ResourceType,
-  timeStamp: number,
-  originUrl: string,
-  statusLine: string,
-  responseHeaders?: browser.webRequest.HttpHeaders,
-  statusCode: number,
+  requestId: string;
+  url: string;
+  method: string;
+  frameId: number;
+  parentFrameId: number;
+  tabId: number;
+  type: browser.webRequest.ResourceType;
+  timeStamp: number;
+  originUrl: string;
+  statusLine: string;
+  responseHeaders?: browser.webRequest.HttpHeaders;
+  statusCode: number;
 }
 
 function listener(details: RequestDetails) {
@@ -26,7 +28,8 @@ function listener(details: RequestDetails) {
   const filter = browser.webRequest.filterResponseData(details.requestId);
 
   // TODO: figure out encoding I guess
-  const enc = new TextDecoder("utf-8");
+  const dec = new TextDecoder("utf-8");
+  const enc = new TextEncoder();
   let content = "";
 
   function disconnect() {
@@ -46,19 +49,30 @@ function listener(details: RequestDetails) {
   };
 
   filter.ondata = (event: Event & {
-    data: ArrayBuffer
+    data: ArrayBuffer;
   }) => {
     console.log(event.data, event);
-    filter.write(event.data);
     if (jsonRequestIds.has(requestId)) {
       console.log("JSONView YEAH data");
     }
-    content = content + enc.decode(event.data);
+    content = content + dec.decode(event.data);
   };
 
   filter.onstop = (event: Event) => {
     console.log("JSONView finished", event, details);
     console.log("CONTENT", content);
+
+    let outputDoc = '';
+
+    try {
+      const jsonObj = JSON.parse(safeStringEncodeNums(content));
+      outputDoc = jsonToHTML(jsonObj, details.url);
+    } catch (e) {
+      outputDoc = errorPage(e, content, details.url);
+    }
+
+    filter.write(enc.encode(outputDoc));
+
     disconnect();
   };
 }
@@ -71,6 +85,8 @@ function detectJSON(event: RequestDetails) {
   for (const header of event.responseHeaders) {
     if (header.name === "Content-Type" && header.value && header.value.includes("application/json")) {
       console.log("JSONView found some JSON!");
+      // TODO: replace headers
+      header.value = "text/html";
       jsonRequestIds.add(event.requestId);
       listener(event);
     }
@@ -83,6 +99,62 @@ function detectJSON(event: RequestDetails) {
 // Set "blocking" and "responseHeaders".
 browser.webRequest.onHeadersReceived.addListener(
   detectJSON,
-  { urls: ["https://jsonview.com/*"], types: ["main_frame"] },
+  { urls: ["<all_urls>"], types: ["main_frame"] },
   ["blocking", "responseHeaders"]
 );
+
+// TODO: as far as I can tell, there's no way to intercept local files
+
+/*
+  *  Takes a JSON string and replaces number values with strings with a leading \u200B.
+  *  Prior to this, it doubles any pre-existing \u200B characters. This Unicode value is
+  *  a zero-width space, so doubling it won't affect the HTML view.
+  *
+  *  This addresses JSONView issue 21 (https://github.com/bhollis/jsonview/issues/21),
+  *  where numbers larger than Number.MAX_SAFE_INTEGER get rounded to the nearest value
+  *  that can fit in the mantissa. Instead we will string encode those numbers, and rely
+  *  on JSONFormatter to detect the leading zero-width space, check the remainder of the
+  *  string with !isNaN() for number-ness, and render it with number styling, sans-quotes.
+  */
+function safeStringEncodeNums(jsonString: string) {
+  const viewString = jsonString.replace(/\u200B/g, "\u200B\u200B");
+
+  // This has some memory of what its last state was
+  let wasInQuotes = false;
+  function isInsideQuotes(str: string) {
+    let inQuotes = false;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === '"') {
+        let escaped = false;
+        for (let lookback = i - 1; lookback >= 0; lookback--) {
+          if (str[lookback] === '\\') {
+            escaped = !escaped;
+          } else {
+            break;
+          }
+        }
+        if (!escaped) {
+          inQuotes = !inQuotes;
+        }
+      }
+    }
+    if (wasInQuotes) {
+      inQuotes = !inQuotes;
+    }
+    wasInQuotes = inQuotes;
+    return inQuotes;
+  }
+
+  let startIndex = 0;
+  function replaceNumbers(match: string, index: number) {
+    // Substring should be copy-on-write, and thus cheap
+    const lookback = viewString.substring(startIndex, index);
+    const insideQuotes = isInsideQuotes(lookback);
+    startIndex = index + match.length;
+    return insideQuotes ? match : `"\u200B${match}"`;
+  }
+
+  // JSON legal number matcher, Andrew Cheong, http://stackoverflow.com/questions/13340717
+  const numberFinder = /-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/g;
+  return viewString.replace(numberFinder, replaceNumbers);
+}
